83bc7790f3c2997b345c4ad7b19c51a793d3247c
Alexander <s1ngleez@yandex.ru>
2024-10-27 21:28:05 +0300

    fix a strong bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,21 @@ import WeatherDay from '../WeatherDay';
 import Input from '../Input';
 
 export default function App() {
+	const token = 'c5ae7fcb86c8427ba1d91415242710',
+		  city = 'Zelenogorsk/Krasnoyarsk/Russia';
+
+	fetch(`https://api.weatherapi.com/v1/forecast.json?key=${token}&q=${city}&days=6`)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok')
+			}
+
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+		})
+
 	return (
 		<SaveDataProvider>
 			<Header />
